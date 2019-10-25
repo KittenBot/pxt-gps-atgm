80b08d2f4d5abf172adc72af285eb6201fb6799a
Riven <4771215@qq.com>
2019-10-26 00:02:18 +0800

    add mars position translate

--- main.ts
+++ main.ts
@@ -7,6 +7,10 @@ load dependency
 //% color="#4c6ef5" weight=10 icon="\uf0ac"
 namespace gps {
 
+    const pi = 3.141592653589793
+    const a = 6378245
+    const ee = 0.006693421622965943
+
     let timezone = +8;
     let hh: number;
     let mm: number;
@@ -15,10 +19,16 @@ namespace gps {
     let MM: number;
     let dd: number;
 
-    let logitude:number;
+    let longitude:number;
     let latitude:number;
     let dir: number;
     let speed: number;
+
+    let sqrtMagic = 0
+    let magic = 0
+    let radLat = 0
+    let mgLat: number;
+    let mgLon: number;
 
     type EvtAct = () => void;
     let gpsGotPosition: EvtAct = null;
@@ -61,6 +71,37 @@ namespace gps {
         return b;
     }
 
+    function transformLat(x: number, y: number): number {
+        let ret = -100.0 + 2.0 * x + 3.0 * y + 0.2 * y * y + 0.1 * x * y
+            + 0.2 * Math.sqrt(Math.abs(x));
+        ret += (20.0 * Math.sin(6.0 * x * pi) + 20.0 * Math.sin(2.0 * x * pi)) * 2.0 / 3.0;
+        ret += (20.0 * Math.sin(y * pi) + 40.0 * Math.sin(y / 3.0 * pi)) * 2.0 / 3.0;
+        ret += (160.0 * Math.sin(y / 12.0 * pi) + 320 * Math.sin(y * pi / 30.0)) * 2.0 / 3.0;
+        return ret;
+    }
+    function transformLon(x: number, y: number): number {
+        let ret2 = 300.0 + x + 2.0 * y + 0.1 * x * x + 0.1 * x * y + 0.1
+            * Math.sqrt(Math.abs(x));
+        ret2 += (20.0 * Math.sin(6.0 * x * pi) + 20.0 * Math.sin(2.0 * x * pi)) * 2.0 / 3.0;
+        ret2 += (20.0 * Math.sin(x * pi) + 40.0 * Math.sin(x / 3.0 * pi)) * 2.0 / 3.0;
+        ret2 += (150.0 * Math.sin(x / 12.0 * pi) + 300.0 * Math.sin(x / 30.0
+            * pi)) * 2.0 / 3.0;
+        return ret2;
+    }
+
+    function gps84Togcj02 (lat: number, lon: number) {
+        let dLat = transformLat(lon - 105.0, lat - 35.0);
+        let dLon = transformLon(lon - 105.0, lat - 35.0);
+        radLat = lat / 180 * pi
+        magic = Math.sin(radLat)
+        magic = 1 - ee * magic * magic
+        sqrtMagic = Math.sqrt(magic)
+        dLat = dLat * 180 / (a * (1 - ee) / (magic * sqrtMagic) * pi)
+        dLon = dLon * 180 / (a / sqrtMagic * Math.cos(radLat) * pi)
+        mgLat = lat + dLat
+        mgLon = lon + dLon
+    }
+
     /**
      * init serial port
      * @param tx Tx pin; eg: SerialPin.P1
@@ -100,10 +141,10 @@ namespace gps {
                 
                 let mode = tmp[2]
                 if (mode == "A"){
-                    logitude = calcgps(tmp[3])
-                    if (tmp[4] == 'S') logitude = -logitude;
-                    latitude = calcgps(tmp[5])
-                    if (tmp[6] == 'W') latitude = -latitude;
+                    latitude = calcgps(tmp[3])
+                    if (tmp[4] == 'S') latitude = -latitude;
+                    longitude = calcgps(tmp[5])
+                    if (tmp[6] == 'W') longitude = -longitude;
                     speed = parseFloat(tmp[7]) * 1.852
                     dir = parseFloat(tmp[8])
 
@@ -114,7 +155,7 @@ namespace gps {
                 }
                 if (gpsGotPosition) gpsGotPosition();
                 //console.log(`time: ${yy}/${MM}/${dd} ${hh}:${mm}:${ss}`)
-                //console.log(logitude + "," + latitude)
+                //console.log(longitude + "," + latitude)
             }
         }
     })
@@ -170,7 +211,26 @@ namespace gps {
             case PosType.Latitude:
                 return latitude;
             case PosType.Longitude:
-                return logitude;
+                return longitude;
+        }
+    }
+
+    //% blockId=gps_to_mars block="GPS Translate to Mars position"
+    //% weight=80
+    //% advanced=true
+    export function gps_to_mars() {
+        gps84Togcj02(latitude, longitude)
+    }
+
+    //% blockId=gps_position_mars block="Read GPS Mars Position%pos"
+    //% weight=80
+    //% advanced=true
+    export function gps_position_mars(pos: PosType): number {
+        switch (pos) {
+            case PosType.Latitude:
+                return mgLat;
+            case PosType.Longitude:
+                return mgLon;
         }
     }
 
